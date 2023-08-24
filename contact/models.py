b0aec8a3b564bb68333f6e38dae74f33b836aea3
revike <revike@ya.ru>
2023-08-24 18:51:37 +0300

    create contact app

---
+++ contact/models.py
@@ -0,0 +1,44 @@
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from base.models import BaseModel, NULLABLE
+from contact.validators import validate_phone
+
+
+class Contact(BaseModel):
+    """Contact model"""
+    first_name = models.CharField(_('first name'), max_length=64, **NULLABLE)
+    last_name = models.CharField(_('last name'), max_length=64, **NULLABLE)
+    phone = models.CharField(_('phone'), max_length=16, validators=[validate_phone])
+    is_active = models.BooleanField(_('is active'), default=True)
+
+    def __str__(self):
+        if not self.first_name and not self.last_name:
+            return f'{self.phone}'
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'contact'
+        verbose_name_plural = 'contacts'
+
+
+class ContactData(BaseModel):
+    """Contact Data model"""
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE,
+                                   related_name='contact_data', verbose_name='contact')
+    email = models.EmailField(_('email'), **NULLABLE)
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'contact data'
+        verbose_name_plural = 'contacts data'
+
+    @staticmethod
+    @receiver(post_save, sender=Contact)
+    def create_contact_data(sender, instance, created, **kwargs):
+        if created and sender == Contact and kwargs:
+            ContactData.objects.create(contact=instance)
