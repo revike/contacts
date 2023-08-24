c584bbadcc81dec05cae7bfc79191f25e10df12f
revike <revike@ya.ru>
2023-08-24 21:39:26 +0300

    contact detail; contact update

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from contact.views import ContactListApiView
+from contact.views import ContactListApiView, ContactDetailApiView
 
 app_name = 'contact'
 
 urlpatterns = [
     path('list/', ContactListApiView.as_view(), name='list'),
+    path('<int:pk>/', ContactDetailApiView.as_view(), name='detail'),
 ]
