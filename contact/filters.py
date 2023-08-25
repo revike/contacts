a9e788b095ceaf4667bd39111b6a4e1225903510
revike <revike@ya.ru>
2023-08-26 01:21:03 +0300

    add sort contacts list

--- contact/filters.py
+++ contact/filters.py
@@ -1,10 +1,14 @@
-from django_filters import FilterSet, CharFilter
+from django.utils.translation import gettext_lazy
+from django.core.exceptions import FieldError
 from django.db.models import Q
+from django_filters import FilterSet, CharFilter
+from rest_framework.exceptions import ValidationError
 
 
 class ContactFilter(FilterSet):
     """Filter for contacts"""
     search = CharFilter(method='search_filter')
+    sort = CharFilter(method='sort_filter')
 
     @classmethod
     def search_filter(cls, queryset, _, value):
@@ -15,4 +19,14 @@ class ContactFilter(FilterSet):
                 filter_list = filter_list | Q(first_name__icontains=obj) | Q(last_name__icontains=obj)
             return queryset.filter(
                 Q(first_name__icontains=value) | Q(last_name__icontains=value) | filter_list).distinct()
+        return queryset
+
+    @classmethod
+    def sort_filter(cls, queryset, _, value):
+        if value:
+            try:
+                return queryset.order_by(value)
+            except FieldError as error:
+                message = gettext_lazy('In reverse order with -.Example, sort=-created.')
+                raise ValidationError({'detail': f'{error} {message}'})
         return queryset
